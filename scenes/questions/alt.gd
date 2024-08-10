ac38571322c6194fb69941d27704a0291f348bf5
Aldo <40561575+adventurer-aldo@users.noreply.github.com>
2024-08-10 12:43:52 +0200

    decisionsfff

--- scenes/questions/alt.gd
+++ scenes/questions/alt.gd
@@ -1,13 +1,15 @@
-extends TextEdit
+extends HBoxContainer
 
-var last_style = load("res://scenes/questions/alt_question_last.tres")
-var regular_style = load("res://scenes/questions/alt_question.tres")
+var last_style = load("res://scenes/questions/alt_last.tres")
+var regular_style = load("res://scenes/questions/alt.tres")
+var last_delete = load("res://scenes/questions/alt_delete_last.tres")
+var regular_delete = load("res://scenes/questions/alt_delete.tres")
 
 func _ready() -> void:
 	texture_by_pos()
 
 func texture_by_pos():
 	if get_parent().get_children().find(self) == get_parent().get_children().size() - 1:
-		add_theme_stylebox_override("normal", last_style)
+		$Alt.add_theme_stylebox_override("normal", last_style)
 	else:
-		add_theme_stylebox_override("normal", regular_style)
+		$Alt.add_theme_stylebox_override("normal", regular_style)
